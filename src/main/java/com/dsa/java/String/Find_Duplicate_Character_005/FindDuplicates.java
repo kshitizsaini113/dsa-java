88ebb8aef93a0d7677cde68ebdfcb6a6bc3b507f
kshitizsaini113 <kshitizsaini.rtk@gmail.com>
2023-10-02 15:02:21 +0530

    Duplicate Character in string using hasing and nested loop

--- src/main/java/com/dsa/java/String/Find_Duplicate_Character_005/FindDuplicates.java
+++ src/main/java/com/dsa/java/String/Find_Duplicate_Character_005/FindDuplicates.java
@@ -28,10 +28,37 @@ public class FindDuplicates {
         duplicateList.forEach(buffer::append);
         return buffer.array();
     }
+
+    public char[] findDuplicatesHashing(String str) {
+        List<Character> duplicateList = new ArrayList<>();
+        char[] hashArray = new char[26];
+
+        for(int i=0; i<str.length(); i++) {
+            if(str.charAt(i)<='Z' && str.charAt(i)>='A') {
+                hashArray[str.charAt(i)-'A']++;
+            } else if(str.charAt(i)<='z' && str.charAt(i)>='a') {
+                hashArray[str.charAt(i)-'a']++;
+            }
+        }
+
+        for(int i=0; i<26; i++) {
+            if(hashArray[i]>1) {
+                duplicateList.add((char) (i+'a'));
+            }
+        }
+
+        CharBuffer charBuffer = CharBuffer.allocate(duplicateList.size());
+        duplicateList.forEach(charBuffer::append);
+        return charBuffer.array();
+    }
+
     public static void main(String[] args) {
         FindDuplicates findDuplicates = new FindDuplicates();
         String str1 = "hellohihola";
         System.out.println(str1);
         System.out.println(Arrays.toString(findDuplicates.findDuplicates(str1)));
+
+        System.out.println(str1);
+        System.out.println(Arrays.toString(findDuplicates.findDuplicatesHashing(str1)));
     }
 }
