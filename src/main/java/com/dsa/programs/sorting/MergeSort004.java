c362435406cbdb9e65269f4e92c01cf551c38602
kshitizsaini113 <kshitizsaini.rtk@gmail.com>
2024-01-07 20:31:32 +0530

    Merge Sort

---
+++ src/main/java/com/dsa/programs/sorting/MergeSort004.java
@@ -0,0 +1,55 @@
+package com.dsa.programs.sorting;
+
+import java.util.Arrays;
+
+public class MergeSort004 {
+    public static void sort(int arr[], int low, int high)
+    {
+        if (low < high) {
+            int mid = low + (high - low) / 2;
+            sort(arr, low, mid);
+            sort(arr, mid + 1, high);
+            merge(arr, low, mid, high);
+        }
+    }
+
+    public static void merge(int arr[], int low, int mid, int high)
+    {
+        int[] tempArr = new int[high-low+1];
+        int idx = 0;
+        int left = low;
+        int right=mid+1;
+        while(left<=mid && right<=high) {
+            if(arr[left]<arr[right]) {
+                tempArr[idx] = arr[left];
+                left++;
+                idx++;
+            } else {
+                tempArr[idx] = arr[right];
+                right++;
+                idx++;
+            }
+        }
+        while(left<=mid) {
+            tempArr[idx] = arr[left];
+            left++;
+            idx++;
+        }
+        while(right<=high) {
+            tempArr[idx] = arr[right];
+            right++;
+            idx++;
+        }
+        System.out.println(Arrays.toString(tempArr));
+        for(int i=0; i<idx; i++) {
+            arr[i+low] = tempArr[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{9, 5,1, 3, 6, 2, 7, 1 ,4};
+        System.out.println(Arrays.toString(arr));
+        sort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+}
